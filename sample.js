94882abb8b21b8cffd3f7ffa75a4885afbdd09a0
sanalmohmd <sanalmuhammed12@gmail.com>
2022-11-17 12:11:27 +0530

    initial commit

---
+++ sample.js
@@ -0,0 +1,25 @@
+// const number = parseInt(prompt("enter a number"));
+
+// let sum = 0;
+
+// for(let i = 1 ; i <=numnber; i++){
+
+//  sum += i;
+
+// }
+
+// document.write("sum of natural number:" + sum  );
+
+
+
+
+const number = parseInt(prompt('Enter a positive integer: '));
+
+let sum = 0;
+
+
+for (let i = 1; i <= number; i++) {
+    sum += i;
+}
+
+document.write('The sum of natural numbers:', sum);
